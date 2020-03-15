b60ffcc7926c2dc48cdff8caa8104d5a1e1f89b1
James Yeates <jimibue@gmail.com>
2020-03-15 09:52:31 -0600

    setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Header, Button, Segment, Card, Icon } from 'semantic-ui-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <Header as="h1" textAlign="center">My Portfolio</Header>
+    <Segment>
+      <Header as="h2" textAlign="center">My Projects</Header>
+    </Segment>
+    <Segment>
+      <Header as="h2" textAlign="center">Contact</Header>
+    </Segment>
+  </div>
+)
 
 export default App;
